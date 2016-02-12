e00c86ddbf6fa757532f1904edf017ddd5fe5ff6
Nishan <roachx@gmail.com>
2016-02-11 19:27:08 -0800

    started select server

---
+++ client_t.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+from _thread import *
+
+DEBUG = True;
+
+
+def print_d(message, debug=True):
+    """Prints message if debug is true."""
+    if debug:
+        print(message, file=sys.stderr)
+
+
+def client_thread(address):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print_d('connecting to %s port %s' % address, DEBUG)
+    sock.connect(address)
+    try:
+        message = 'This is the message.  It will be repeated.'
+        while True:
+            print_d('sending "%s"' % message, DEBUG)
+            sock.sendall(message.encode('utf-8'))
+            amount_received = 0
+            amount_expected = len(message)
+            while amount_received < amount_expected:
+                data = sock.recv(1024)
+                amount_received += len(data)
+                print_d('received "%s"' % data, DEBUG);
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    server_address = ('localhost', 10000)
+    # server_address = (sys.argv[1], 10000)
+    print_d('i iz rnning')
+    for x in range(1, 5):
+        print_d('i iz looping')
+        start_new_thread(client_thread, (server_address,))
---
+++ server_s.py
@@ -0,0 +1,53 @@
+import select
+from  socket import *
+import sys
+import queue
+
+
+def print_d(message, debug=True):
+    """Prints message if debug is true."""
+    if debug:
+        print(message, file=sys.stderr)
+
+
+server_address = ('', 10000)
+listen_socket = socket(AF_INET, SOCK_STREAM)
+listen_socket.setblocking(0)
+listen_socket.bind(server_address)
+listen_socket.listen(5)
+
+# Sockets from which we expect to read
+inputs = [listen_socket]
+
+# Sockets to which we expect to write
+outputs = []
+
+# Outgoing message queues (socket:Queue)
+message_queues = {}
+
+while inputs:
+    # Wait for at least one of the sockets to be ready for processing
+    print_d('\nwaiting for the next event')
+    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+    for s in readable:
+
+        if s is listen_socket:
+            # A "readable" server socket is ready to accept a connection
+            connection, client_address = s.accept()
+            print_d('...connected from:' + str(client_address), True)
+            connection.setblocking(0)
+            inputs.append(connection)
+
+            # Give the connection a queue for data we want to send
+            message_queues[connection] = queue.Queue()
+
+        else:
+            data = s.recv(1024)
+            if data:
+                # A readable client socket has data
+                # print >>sys.stderr, 'received "%s" from %s' % (data, s.getpeername())
+                message_queues[s].put(data)
+                # Add output channel for response
+                if s not in outputs:
+                    outputs.append(s)
