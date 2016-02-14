7fdde9c1e6b6ae76822b259af3c2336e1803bafa
Nishan <roachx@gmail.com>
2016-02-13 20:24:06 -0800

    rewrote multithreaded server

---
+++ server_t2.py
@@ -0,0 +1,57 @@
+from socket import *
+import threading
+import sys
+
+DEBUG = True
+LISTEN_PORT = 10000
+BUFFER_SIZE = 1024
+
+
+def print_d(message, debug=True):
+    """Prints message if debug is true."""
+    if debug:
+        print(message, file=sys.stderr)
+
+
+class Client_HandlerThread(threading.Thread):
+
+    def __init__(self, client_socket, client_address):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.client_address = client_address
+        print_d("Thread created for {0}".format(client_address))
+
+    def run(self):
+        while 1:
+            try:
+                data = self.client_socket.recv(BUFFER_SIZE)
+                data_string = data.decode()
+                if data:
+                    print("Received: {0} ".format(data_string) + " from {0} ".format(self.client_address))
+                    self.client_socket.sendall(data)
+                else: #connection closed
+                    print_d("Closing connection with {0}, no data".format(self.client_address))
+                    self.client_socket.close()
+                    break
+            except Exception:
+                    print_d("Closing connection with {0}, due to exception".format(self.client_address))
+                    self.client_socket.close()
+                    break
+        self.client_socket.close()
+        print_d("Thread ending for {0}".format(self.client_address))
+
+
+# Setup Listening Socket
+server_address = ('', LISTEN_PORT)
+listen_socket = socket(AF_INET, SOCK_STREAM)
+# listen_socket.setblocking(0)
+listen_socket.bind(server_address)
+
+#Main loop
+while 1:
+    listen_socket.listen(5)
+    # print_d ("Waiting for connection")
+    client_socket, client_address = listen_socket.accept()
+    newClientThread = Client_HandlerThread(client_socket, client_address)
+    newClientThread.start()
+
