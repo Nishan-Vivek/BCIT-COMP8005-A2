9258100ff8b70fb0567e0c7cddb600ef818147f5
Nishan <roachx@gmail.com>
2016-02-14 23:51:17 -0800

    Starting epoll server

--- client_pt.py
+++ client_pt.py
@@ -3,13 +3,16 @@ import sys
 from multiprocessing import Pool, Process
 import threading
 import os
+import time
+import csv
+import datetime
 
-DEBUG = False
+DEBUG = True
 SERVER_PORT = 10000
 SERVER_ADDRESS = 'localhost'
-PROC_NUM = 1000
+PROC_NUM = 1
 THREAD_PER_PROC = 1
-REPEAT = 1000
+REPEAT = 10
 SOCKET_TIMEOUT = 10
 PAYLOAD = "This is the payload"
 
@@ -23,27 +26,34 @@ def print_d(message, debug=True):
 def getClientID():
     return "PID:{0}".format(os.getpid()) + "-" + threading.current_thread().getName()
 
+
 def messaging2(sock, message):
     stats = ClientStats()
     stats.client_id = getClientID()
     stats.req_w = REPEAT
-    stats.req_c = 0
-    stats.data_transferred = 0
-
+    stats.req_c += 1
     try:
-
+        starttime = time.time()
         for x in range(REPEAT):
             print_d('sending "%s"' % message, DEBUG)
             sock.sendall(message.encode())
             data = sock.recv(1024).decode()
             if data:
-                stats.req_c +=1
-                stats.data_transferred += sys.getsizeof(data)
+                stats.req_c += 1
+                stats.data_transferred += sys.getsizeof(data).to_bytes()
                 print_d('received "%s"' % data, DEBUG)
     except:
+        endtime = time.time()
+        totalTime = endtime - starttime
+        if stats.req_c != 0:
+            stats.avg_rtt = totalTime / stats.req_c
         return stats
         raise
     finally:
+        endtime = time.time()
+        totalTime = endtime - starttime
+        if stats.req_c != None:
+            stats.avg_rtt = totalTime / stats.req_c
         return stats
 
 
@@ -56,22 +66,23 @@ def messaging(sock, message):
     except:
         raise
 
+
 class ClientStats():
-    def __init__(self, client_id, req_c, req_w, avg_rtt, data_sent):
-        self.client_id = client_id
-        self.req_c = req_c
-        self.req_w = req_w
-        self.data_sent = data_sent
-        self.avg_rtt = avg_rtt
+    def __init__(self):
+        self.client_id = 0
+        self.req_c = 0
+        self.req_w = 0
+        self.data_sent = 0
+        self.avg_rtt = 0
 
 
 class ClientThread(threading.Thread):
     def __init__(self, server_address):
         threading.Thread.__init__(self)
         self.server_address = server_address
+        self.statistics = {}
 
     def run(self):
-
         try:
             # Create a TCP/IP socket
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -83,15 +94,15 @@ class ClientThread(threading.Thread):
             sock.connect(self.server_address)
             message = PAYLOAD
 
-            #Send the payload the REPEAT number of times or indefitealy if REPEAT = 0
+            # Send the payload the REPEAT number of times or indefitealy if REPEAT = 0
             if REPEAT == 0:
                 while 1:
                     messaging(sock, message)
             else:
                 for x in range(REPEAT):
-                    messaging(sock, message)
+                    self.statistics[getClientID()] = messaging2(sock, message)
 
-        #close sockets on exceptions and completion
+        # close sockets on exceptions and completion
         except Exception as e:
             print_d(
                 "PID:{0}".format(os.getpid()) + "-" + threading.current_thread().getName() + " encountered " + repr(e))
@@ -101,23 +112,45 @@ class ClientThread(threading.Thread):
 
         print_d("PID:{0}".format(os.getpid()) + "-" + threading.current_thread().getName() + " ending Thread")
 
+    def join(self, *args, **kwargs):
+        super(ClientThread, self).join(*args, **kwargs)
+        return self.statistics
+
 
 def client_process(*args):
     print_d("PID:{0}".format(os.getpid()) + " created.")
     threads = []
     server_address = (sys.argv[1], 10000)
+    statistics = {}
 
     for _ in range(THREAD_PER_PROC):
         threads.append(ClientThread(server_address))
 
     [x.start() for x in threads]
-    [x.join() for x in threads]
+
+    for x in threads:
+        x.join()
+        statistics.update(x.statistics)
+
     print_d("PID:{0}".format(os.getpid()) + " Ending Process")
+
+    return statistics
 
 
 if __name__ == '__main__':
+    statistics = {}
     pool = Pool(processes=PROC_NUM)
     results = pool.map(client_process, range(PROC_NUM))
     pool.close()
     pool.join()
     print_d("All processes ended")
+    # print_d(type(results))
+    for x in results:
+        statistics.update(x)
+    print_d("Writing statistics to clientStats.csv")
+    with open('clientStats.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, dialect='excel')
+        filewriter.writerow(["ClientID", "Completed Connections", "Wanted Connections", "Data Sent", "Average RTT"])
+        for x in statistics:
+            filewriter.writerow([statistics[x].client_id, statistics[x].req_c, statistics[x].req_w, statistics[x].data_sent, statistics[x].avg_rtt])
+
